e997d25f0d64940753bbe5b7a4f1b1428148d2e3
Saleh Shakib <salehshakib.ss@gmail.com>
2025-07-24 03:39:40 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/ui/meteors.tsx
+++ src/components/ui/meteors.tsx
@@ -8,10 +8,21 @@ export const Meteors = ({
   number?: number;
   className?: string;
 }) => {
-  const meteors = new Array(number || 20).fill(true);
+  const [meteorStyles, setMeteorStyles] = useState<React.CSSProperties[]>([]);
+
+  useEffect(() => {
+    const styles = new Array(number || 20).fill(true).map(() => ({
+      top: 0,
+      left: Math.floor(Math.random() * (400 - -400) + -400) + "px",
+      animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
+      animationDuration: Math.floor(Math.random() * (10 - 2) + 2) + "s",
+    }));
+    setMeteorStyles(styles);
+  }, [number]);
+
   return (
     <>
-      {meteors.map((el, idx) => (
+      {meteorStyles.map((style, idx) => (
         <span
           key={"meteor" + idx}
           className={cn(
@@ -19,12 +30,7 @@ export const Meteors = ({
             "before:content-[''] before:absolute before:top-1/2 before:transform before:-translate-y-[50%] before:w-[50px] before:h-[1px] before:bg-gradient-to-r before:from-[#64748b] before:to-transparent",
             className
           )}
-          style={{
-            top: 0,
-            left: Math.floor(Math.random() * (400 - -400) + -400) + "px",
-            animationDelay: Math.random() * (0.8 - 0.2) + 0.2 + "s",
-            animationDuration: Math.floor(Math.random() * (10 - 2) + 2) + "s",
-          }}
+          style={style}
         ></span>
       ))}
     </>
